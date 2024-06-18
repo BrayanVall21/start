11f87d70b9c05def223870a29e9055a08b73173f
Jorge Ramírez <jorge.ramirezgarciap@gmail.com>
2024-06-18 13:21:34 -0500

    Start v08

--- src/data/interpretResponses.js
+++ src/data/interpretResponses.js
@@ -31,7 +31,37 @@ const sendToGemini = async (responses, type) => {
     }
     else{
       geminiResponse = await fetchGeminiResponse(
-        `Estas son las respuestas negativas de la startup: ${JSON.stringify(responses)}. Dame una breve recomendación de mejora para ellas. (La respuesta que debes darme es un texto breve de corrido)`
+        `Estas son las respuestas negativas de la startup: ${JSON.stringify(responses)}. Dame una recomendación de mejora para ellas; además, considera que para las siguientes preguntas debes añadir estas métricas en tus respuestas.
+        Si la pregunta es ¿Tiene alguna métrica de medición de satisfacción del cliente en su startup? debes recomendarle que es recomendable tener indicadores de rendimiento que permitan conocer la satisfacción del cliente, además de indicadores como el ingreso promedio por usuario, la tasa de abandono de clientes y el valor de vida del cliente.
+        Si la pregunta es ¿Su startup tiene financiamiento (Propia/externa)? debes recomendarlo este sitio oficial del estado peruano en el que se listan todos los concursos de financiamiento existentes en el Perú https://calendario.proinnovate.gob.pe/
+        Si la pregunta es ¿Está siendo incubado en alguna incubadora de empresas? debes recomendarle este directorio de todas las incubadoras del Perú https://startup.proinnovate.gob.pe/wp-content/uploads/2021/12/20122021-Directorio-Incubadoras-y-Aceleradoras-Red-ProInnovate-2021.pdf
+        Si la pregunta es ¿El ecosistema actual de su startup presenta factores de innovación y emprendimiento que hayan sido útiles para su desarrollo? debes recomendable incluir los siguientes factores de innovación Conciencia Empresarial y Motivación, Educación y Formación, Necesidad de Logro, Decisiones Empresariales, Capacidades de Innovación Estratégica, Dinámicas de Innovación y Emprendimiento en el Ámbito Académico
+        Si la pregunta es ¿Tiene experiencia en creación de empresas? recomendarle los siguientes cursos gratuitos del estado https://mtpe.trabajo.gob.pe/capacitacionlaboral/como-emprender/ https://www.gob.pe/tuempresa
+        
+        Si la pregunta es ¿Cuenta con habilidades técnicas y empresariales? recomendarle tener en cuenta los siguientes tipos de habilidades empresariales: personales, interpersonales y grupales
+        
+        Si la pregunta es ¿Tiene experiencia en la gestión empresarial? recomendarle los siguientes cursos https://www.udemy.com/es/courses/business/management/ 
+        https://usil.edu.pe/posgrado/educacion-ejecutiva/gestion-y-emprendimiento/administracion-y-gestion-de-empresas 
+        https://www.esan.edu.pe/pee/empresarial/gestion-y-finanzas-para-startups-emprendedores/308
+        Si la pregunta es ¿Ha recibido o está recibiendo apoyo gubernamental (Económico, Tutoría, etc)? recomendar los siguientes concursos de apoyo al emprendedor  https://startup.proinnovate.gob.pe/ 
+        
+        https://www.cofide.com.pe/apoyo_mipyme.php
+        
+        Si la pregunta es ¿Los productos/servicios que están planteando son innovadores? recomendar tomar en cuenta las siguientes Clases de innovación: 
+        -de producto
+        -de proceso
+        -de organización
+        -de marketing
+        
+        Tipos:
+        -Radical
+        -incremental
+        -arquitectural
+        -conceptual
+        
+        Si la pregunta es ¿Qué tanto influyen las políticas estatales científicas y tecnológicas en tu start up? recomendar el enlace a la ley del joven emprendedor https://busquedas.elperuano.pe/dispositivo/NL/2195233-1
+        
+        Para las preguntas que no especifiqué, usa tu criterio al recomendar`
       );
     }
     return geminiResponse;
--- src/screens/PostResultados.js
+++ src/screens/PostResultados.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import evaluarExito from '../data/evaluacion';
 import questions1 from '../data/opcion1/questions';
@@ -24,11 +24,10 @@ const ResultsScreen = () => {
 
     if (answers) {
       (async () => {
-        const API_KEY = 'Tu-API-Key-Aquí'; // Asegúrate de usar tu clave API real
         const result = await evaluarExito(answers);
         setExito(result);
         try {
-          const interpretationResult = await interpretResponses(answers, totalQuestions, API_KEY);
+          const interpretationResult = await interpretResponses(answers, totalQuestions);
           setInterpretation(interpretationResult);
         } catch (error) {
           console.error("Error al interpretar las respuestas: ", error);
@@ -50,30 +49,38 @@ const ResultsScreen = () => {
   };
 
   return (
-    <LinearGradient colors={theme === 'dark' ? ["#070F2B", "#1B1A55", "#535C91"] : ["#1B1A55", "#535C91", "#9290C3"]} style={styles.container}>
-      <View style={[styles.container, { backgroundColor: theme === 'dark' ? '#1B1A55' : 'white' }]}>
-        <Text style={[styles.title, { color: theme === 'dark' ? 'white' : 'black' }]}>Resultados</Text>
-        <Text style={[styles.exito, { color: theme === 'dark' ? 'white' : 'black' }]}>Éxito de la startup: {exito}</Text>
-        <Text style={[styles.exito, { color: theme === 'dark' ? 'white' : 'black' }]}>Interpretación de los resultados:</Text>
-        <Text style={[styles.interpretation, { color: theme === 'dark' ? 'white' : 'black' }]}>{interpretation}</Text>
-        
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Finalizar"
-            onPress={handleNavigation}
-            color={theme === 'dark' ? '#FFFFFF' : '#000000'}
-          />
+    <LinearGradient colors={theme === 'dark' ? ["#070F2B", "#1B1A55", "#535C91"] : ["#1B1A55", "#535C91", "#9290C3"]} style={styles.linearGradient}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={[styles.content, { backgroundColor: theme === 'dark' ? '#1B1A55' : 'white' }]}>
+          <Text style={[styles.title, { color: theme === 'dark' ? 'white' : 'black' }]}>Resultados</Text>
+          <Text style={[styles.exito, { color: theme === 'dark' ? 'white' : 'black' }]}>Éxito de la startup: {exito}</Text>
+          <Text style={[styles.exito, { color: theme === 'dark' ? 'white' : 'black' }]}>Interpretación de los resultados:</Text>
+          <Text style={[styles.interpretation, { color: theme === 'dark' ? 'white' : 'black' }]}>{interpretation}</Text>
+          
+          <View style={styles.buttonContainer}>
+            <Button
+              title="Finalizar"
+              onPress={handleNavigation}
+              color={theme === 'dark' ? '#FFFFFF' : '#000000'}
+            />
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </LinearGradient>
   );
 };
 
 const styling = theme => 
   StyleSheet.create({
-    container: {
+    linearGradient: {
       flex: 1,
+    },
+    container: {
+      flexGrow: 1,
       padding: 20,
+    },
+    content: {
+      flex: 1,
     },
     title: {
       fontSize: 24,
